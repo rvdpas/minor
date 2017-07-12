93c16ef48ed05563a0190e71285b72c3677252d4
rvdpas <rvdpas94@gmail.com>
2017-07-12 15:28:49 +0200

    update links

--- project-1/js/config.js
+++
@@ -1,4 +0,0 @@
-var config = {
-	apiUrl: 'https://funda.kyrandia.nl/feeds/Aanbod.svc/json/271175433a7c4fe2a45750d385dd9bfd/?type=koop&zo=/',
-	autoSuggest: 'http://zb.funda.info/frontend/geo/suggest/?query=',
-}
