adc25403dd727507c1819d345a235240d2f0335e
rvdpas <rvdpas94@gmail.com>
2017-07-12 15:18:59 +0200

    update to https

--- project-1/js/config.js
+++ project-1/js/config.js
@@ -1,4 +1,4 @@
 var config = {
-	apiUrl: 'http://funda.kyrandia.nl/feeds/Aanbod.svc/json/271175433a7c4fe2a45750d385dd9bfd/?type=koop&zo=/',
+	apiUrl: 'https://funda.kyrandia.nl/feeds/Aanbod.svc/json/271175433a7c4fe2a45750d385dd9bfd/?type=koop&zo=/',
 	autoSuggest: 'http://zb.funda.info/frontend/geo/suggest/?query=',
 }
